8d581e7c962beed0804b1df1170adc1d1f8a96a0
gounpyeon <vusrhdns@naver.com>
2023-11-12 16:00:02 +0900

    uploaded

--- tcp-server.py
+++ tcp-server.py
@@ -17,7 +17,7 @@ while 1:
     print('Client address:', addr)
     data = conn.recv(BUFFER_SIZE)
     #if not data: break
-    currentTime = " " + " sended from user !!! " + time.ctime(time.time()) + "\r\n"
+    currentTime = " " + " uploaded !!! " + time.ctime(time.time()) + "\r\n"
     print(data.decode('utf-8'))
     data = data + currentTime.encode('ascii')
     conn.send(data) # echo
